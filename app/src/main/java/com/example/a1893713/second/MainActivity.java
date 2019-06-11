2d6b01a8d554b0a1e7c8c04760cf9ce2d8fa61d0
1893713 <kevalbhaimiyani5@gmail.com>
2019-06-11 09:55:33 -0400

    nevigation

--- app/src/main/java/com/example/a1893713/second/MainActivity.java
+++ app/src/main/java/com/example/a1893713/second/MainActivity.java
@@ -1,29 +1,40 @@
 package com.example.a1893713.second;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText tuser,pass;
+    EditText edt_uname,edt_pass;
+
     Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tuser = findViewById(R.id.txtuser);
-        pass = findViewById(R.id.txtpass);
+        edt_uname = findViewById(R.id.txtuser);
+        edt_pass = findViewById(R.id.txtpass);
         btn = findViewById(R.id.btnpress);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+        String uname,pass;
 
+       uname = edt_uname.getText().toString();
+       pass = edt_pass.getText().toString();
 
+        Intent i = new Intent(MainActivity.this,dashboard.class);
+        i.putExtra("un",uname);
+        i.putExtra("ps",pass);
+        startActivity(i);
 
+                Toast.makeText(getApplicationContext(),uname+"Thanx",Toast.LENGTH_LONG).show();
             }
         });
     }
---
+++ app/src/main/java/com/example/a1893713/second/dashboard.java
@@ -0,0 +1,28 @@
+package com.example.a1893713.second;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class dashboard extends AppCompatActivity {
+String name,pass;
+TextView txt_nm,txt_ps;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        Intent i = getIntent();
+      name =  i.getStringExtra("un");
+      pass =   i.getStringExtra("ps");
+
+
+
+        txt_nm = findViewById(R.id.txtname);
+        txt_ps = findViewById(R.id.txtpass);
+
+        txt_nm.setText(name);
+        txt_ps.setText(pass);
+    }
+}
